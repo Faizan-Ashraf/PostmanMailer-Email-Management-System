784c17da7c5a544e77810717121f89a3b9f8478b
Faizan-Ashraf <faizantyu1177@gmail.com>
2025-01-30 19:38:49 +0500

    User and Emails Schema Created

--- models/emails.js
+++ models/emails.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
-const projectsSchema = new mongoose.Schema({
-    title:{type:String, required:true,unique:true},
-    description:{type:String, required:true},
-    studentId:{type: mongoose.Schema.Types.ObjectId, ref:'users', unique:true},
-    supervisorId:{type: mongoose.Schema.Types.ObjectId, ref:'users'},
-    status:{type:String,enum: ["proposed","completed"], required:true}
+const mailsSchema = new mongoose.Schema({
+    to:{type:String, required:true},
+    from:{type:String, required:true},
+    subject:{type:String,required:true},
+    mail:{type: String, required: true},
 })
 
-const Projects = mongoose.model('Projects',projectsSchema,'projects');
+const Mails = mongoose.model('Mails',projectsSchema,'mails');
 
-export default Projects;
+export default Mails;
